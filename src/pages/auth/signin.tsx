99a2a908b79f7de565b6cb8499079c8b24e21aac
Giorgio Genovese <genovesegiorgio6@gmail.com>
2026-07-03 20:23:42 +0200

    redesign: restyle Login screen provider buttons

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -205,7 +205,7 @@ const Home: NextPage<{
                     .filter((provider) => 'email' !== provider.id)
                     .map((provider) => (
                       <Button
-                        className="mx-auto my-2 flex w-[300px] items-center gap-3 bg-white hover:bg-gray-100 focus:bg-gray-100"
+                        className="mx-auto my-2 flex w-[300px] items-center gap-3 rounded-[0.875rem] bg-white text-black hover:bg-gray-100 focus:bg-gray-100"
                         onClick={handleProviderSignIn(provider.id)}
                         key={provider.id}
                       >
@@ -230,7 +230,7 @@ const Home: NextPage<{
                         >
                           <FormField control={emailForm.control} name="email" render={field} />
                           <Button
-                            className="mt-6 w-[300px] bg-white hover:bg-gray-100 focus:bg-gray-100"
+                            className="mt-6 w-[300px] rounded-[0.875rem] bg-white text-black hover:bg-gray-100 focus:bg-gray-100"
                             type="submit"
                             disabled={'sending' === emailStatus}
                           >
